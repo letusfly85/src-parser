3b3926fd4168eff825693053c9f865e308ab8608
letusfly85 <letusfly95@gmail.com>
2013-11-05 21:40:16 +0900

    gather info from configuration file

--- src/main/groovy/com/jellyfish85/srcParser/parser/ConfigActionParser.groovy
+++ src/main/groovy/com/jellyfish85/srcParser/parser/ConfigActionParser.groovy
@@ -1,9 +1,158 @@
 package com.jellyfish85.srcParser.parser
 
+import com.jellyfish85.dbaccessor.bean.src.mainte.tool.RsConfigAttributesBean
+import com.jellyfish85.srcParser.utils.ApplicationProperties
+import com.jellyfish85.svnaccessor.bean.SVNRequestBean
+import org.apache.commons.io.FilenameUtils
+import org.w3c.dom.Element
+import org.xml.sax.SAXParseException
+
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+
 class ConfigActionParser {
 
     public ConfigActionParser() {}
 
+    public static ArrayList<RsConfigAttributesBean> parse(
+            ApplicationProperties            app,
+            ArrayList<SVNRequestBean>        list
+    ) {
+        def resultSets = new ArrayList<RsConfigAttributesBean>()
 
+        list.each {SVNRequestBean bean ->
+            parse(app, bean).each {RsConfigAttributesBean entry -> resultSets.add(entry)}
+        }
+
+        return resultSets
+    }
+
+    public static ArrayList<RsConfigAttributesBean> parse(
+            ApplicationProperties app,
+            SVNRequestBean        bean
+    ) {
+        def resultSets = new ArrayList<RsConfigAttributesBean>()
+
+        try {
+            def file = new File(app.workspace(), bean.path())
+            if (!file.exists()) {
+                new RuntimeException("file is not exists!")
+            }
+
+            DocumentBuilderFactory factory   = DocumentBuilderFactory.newInstance()
+            DocumentBuilder        db        = factory.newDocumentBuilder()
+            org.w3c.dom.Document   doc       = db.parse(file)
+
+            /*if (file.getName() != "command-config-jyb-tp.xml") {
+                return resultSets
+            }*/
+
+            Element elem                     = doc.getDocumentElement()
+            org.w3c.dom.NodeList nodeList    = elem.getElementsByTagName("command")
+
+            if (nodeList.length == 0) {
+                return resultSets
+            }
+
+            for (int i = 0; i < nodeList.length; i++){
+
+                def node = nodeList.item(i)
+                Element entry  = (Element)node
+
+                def commandName = entry.getAttribute("name")
+
+                def searchNodeList =  entry.getElementsByTagName("search")
+                if (searchNodeList.length > 0) {
+
+                    for (int k = 0; k < searchNodeList.length; k++){
+                        def _node  = searchNodeList.item(k)
+                        def _entry = (Element)_node
+
+                        if (_node != null && _entry.hasAttribute("subjectid")) {
+                            def entity = new RsConfigAttributesBean()
+
+                            entity.headRevisionAttr().setValue(new BigDecimal(bean.headRevision()))
+                            entity.fileNameAttr().setValue(bean.fileName())
+                            entity.pathAttr().setValue(bean.path())
+                            entity.revisionAttr().setValue(new BigDecimal(bean.revision()))
+                            entity.authorAttr().setValue(bean.author())
+                            entity.commitYmdAttr().setValue(bean.commitYmd())
+                            entity.commitHmsAttr().setValue(bean.commitHms())
+                            entity.extensionAttr().setValue(FilenameUtils.getExtension(bean.path()))
+
+                            entity.actionNameAttr().setValue(commandName)
+                            entity.subjectIdAttr().setValue(_entry.getAttribute("subjectid"))
+
+                            resultSets.add(entity)
+                        }
+                    }
+                }
+
+                def excelNodeList =  entry.getElementsByTagName("excel")
+                if (excelNodeList.length > 0) {
+
+                    for (int k = 0; k < excelNodeList.length; k++){
+                        def _node  = excelNodeList.item(k)
+                        def _entry = (Element)_node
+
+                        if (_node != null && _entry.hasAttribute("subjectid")) {
+                            def entity = new RsConfigAttributesBean()
+                            entity.headRevisionAttr().setValue(new BigDecimal(bean.headRevision()))
+                            entity.fileNameAttr().setValue(bean.fileName())
+                            entity.pathAttr().setValue(bean.path())
+                            entity.revisionAttr().setValue(new BigDecimal(bean.revision()))
+                            entity.authorAttr().setValue(bean.author())
+                            entity.commitYmdAttr().setValue(bean.commitYmd())
+                            entity.commitHmsAttr().setValue(bean.commitHms())
+                            entity.extensionAttr().setValue(FilenameUtils.getExtension(bean.path()))
+
+                            entity.actionNameAttr().setValue(commandName)
+                            entity.subjectIdAttr().setValue(_entry.getAttribute("subjectid"))
+
+                            resultSets.add(entity)
+                        }
+                    }
+                }
+
+                def entryNodeList =  entry.getElementsByTagName("entry")
+                if (entryNodeList.length > 0) {
+
+                    for (int k = 0; k < entryNodeList.length; k++){
+                        def _node  = entryNodeList.item(k)
+                        def _entry = (Element)_node
+
+                        if (_node != null && _entry.hasAttribute("subjectid")) {
+                            def entity = new RsConfigAttributesBean()
+                            entity.headRevisionAttr().setValue(new BigDecimal(bean.headRevision()))
+                            entity.fileNameAttr().setValue(bean.fileName())
+                            entity.pathAttr().setValue(bean.path())
+                            entity.revisionAttr().setValue(new BigDecimal(bean.revision()))
+                            entity.authorAttr().setValue(bean.author())
+                            entity.commitYmdAttr().setValue(bean.commitYmd())
+                            entity.commitHmsAttr().setValue(bean.commitHms())
+                            entity.extensionAttr().setValue(FilenameUtils.getExtension(bean.path()))
+
+                            entity.actionNameAttr().setValue(commandName)
+                            entity.subjectIdAttr().setValue(_entry.getAttribute("subjectid"))
+
+                            resultSets.add(entity)
+                        }
+                    }
+                }
+            }
+
+            println("[TARGET]" + bean.path())
+
+        } catch (SAXParseException e) {
+            println("[PARSE-ERROR]" + bean.path())
+
+        } catch (Exception e) {
+            e.printStackTrace()
+            println("[RUNTIME-ERROR]" + bean.path())
+
+        }
+
+        return resultSets
+    }
 
 }
---
+++ src/main/groovy/com/jellyfish85/srcParser/runner/ConfigActionRunner.groovy
@@ -0,0 +1,76 @@
+package com.jellyfish85.srcParser.runner
+
+import com.jellyfish85.dbaccessor.dao.src.mainte.tool.RsConfigAttributesDao
+import com.jellyfish85.dbaccessor.manager.DatabaseManager
+import com.jellyfish85.srcParser.utils.ApplicationProperties
+
+import com.jellyfish85.srcParser.downloader.DownloadSource2Workspace
+import com.jellyfish85.srcParser.parser.ConfigActionParser
+import com.jellyfish85.svnaccessor.bean.SVNRequestBean
+import com.jellyfish85.svnaccessor.getter.SVNGetFiles
+import org.apache.commons.io.FilenameUtils
+
+class ConfigActionRunner {
+
+    public static void main(String[] args) {
+        def db = new DatabaseManager()
+        db.connect()
+
+        def app    = new ApplicationProperties()
+        def dl     = new DownloadSource2Workspace()
+        def parser = new ConfigActionParser()
+
+        def register = new RsConfigAttributesDao()
+        def list     = getConfigFileNames()
+
+        //download all page files and parse them
+        dl.downLoadAll(app, list)
+
+        def modifier = new SVNGetFiles()
+        def _list = modifier.modifyAttribute2Current(list)
+
+        def resultSets = parser.parse(app, _list)
+
+        // insert parse results to database
+        register.deleteAll(db.conn())
+        register.insert(db.conn(), resultSets)
+
+        if (!db.conn().closed) {
+            db.jCommit()
+            db.jClose()
+        }
+    }
+
+    public static ArrayList<SVNRequestBean> getConfigFileNames() {
+        def list = new ArrayList<SVNRequestBean>()
+
+        try {
+            def inputStream = getClass().getResourceAsStream("/configFileName.lst")
+
+            def reader =
+                new BufferedReader(new InputStreamReader(inputStream, "UTF-8"))
+
+            Boolean _switch = true
+            def content = ""
+            while (_switch) {
+                content = reader.readLine()
+                if (content.equals(null)) {
+                    _switch = false
+
+                } else {
+                    def bean = new SVNRequestBean()
+                    bean.path = content
+                    bean.fileName = FilenameUtils.getName(content)
+
+                    list.add(bean)
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace()
+        }
+
+        return list
+    }
+
+}
