74dbc25e26f9f203c83884f855abafe1d85d0bd3
letusfly85 <letusfly95@gmail.com>
2013-11-09 15:43:18 +0900

    add executor class to package

---
+++ src/main/scala/com/jellyfish85/srcParser/executor/ParseTableAttribute.scala
@@ -0,0 +1,22 @@
+package com.jellyfish85.srcParser.executor
+
+import com.jellyfish85.srcParser.parser.TableParser
+
+/**
+ * == ParseTableAttribute ==
+ *
+ *
+ *
+ *@example
+ *    gradle run -Prunargs=com.jellyfish85.srcParser.executor.ParseTableAttribute
+ *
+ * @author wada shunsuke
+ *
+ */
+class ParseTableAttribute {
+
+  def run(args: Array[String]) {
+    println("parse")
+  }
+
+}
--- src/main/scala/com/jellyfish85/srcParser/parser/TableParser.scala
+++ src/main/scala/com/jellyfish85/srcParser/parser/TableParser.scala
@@ -7,4 +7,22 @@ import org.antlr.runtime.tree.CommonTree
 
 class TableParser {
 
+  def parse(sql: String) {
+
+    val stream: ANTLRStringStream = new ANTLRStringStream(sql.toLowerCase)
+
+    val lexer 	= new PLSQLLexer(stream)
+
+    val tokens	= new CommonTokenStream(lexer)
+
+    val parser = new PLSQLParser(tokens)
+
+    val result = parser.sql_statement
+
+    val tree = result.getTree.asInstanceOf[CommonTree]
+
+    println(tree)
+
+  }
+
 }
