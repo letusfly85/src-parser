12fd20232bcd0ad555350b8c1439f417c7320386
letusfly85 <letusfly95@gmail.com>
2013-11-09 14:12:06 +0900

    add a parsing sql itself module

---
+++ src/main/scala/com/jellyfish85/srcParser/parser/TableParser.scala
@@ -0,0 +1,10 @@
+package com.jellyfish85.srcParser.parser
+
+import br.com.porcelli.parser._
+
+import org.antlr.runtime.{CommonTokenStream, ANTLRStringStream}
+import org.antlr.runtime.tree.CommonTree
+
+class TableParser {
+
+}
