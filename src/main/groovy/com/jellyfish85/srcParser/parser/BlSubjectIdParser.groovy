41f5aaaa162828f8033be1951d5ca3666b0fadbd
letusfly85 <letusfly95@gmail.com>
2013-11-21 01:16:44 +0900

    add BlSubjectIdParser class

---
+++ src/main/groovy/com/jellyfish85/srcParser/parser/BlSubjectIdParser.groovy
@@ -0,0 +1,16 @@
+package com.jellyfish85.srcParser.parser
+
+import com.jellyfish85.dbaccessor.bean.src.mainte.tool.RsSubjectidBlpathIdxBean
+import com.jellyfish85.srcParser.utils.ApplicationProperties
+import com.jellyfish85.svnaccessor.bean.SVNRequestBean
+import org.w3c.dom.Element
+import org.xml.sax.SAXParseException
+
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
+
+class BlSubjectIdParser {
+
+
+
+}
