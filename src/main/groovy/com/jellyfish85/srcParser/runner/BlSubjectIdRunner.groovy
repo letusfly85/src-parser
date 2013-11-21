4ecfad0a6e2e13abee2a0cece9988849f534335b
letusfly85 <letusfly95@gmail.com>
2013-11-22 07:12:23 +0900

    add BlSubjectIdRunner

---
+++ src/main/groovy/com/jellyfish85/srcParser/runner/BlSubjectIdRunner.groovy
@@ -0,0 +1,21 @@
+package com.jellyfish85.srcParser.runner
+
+/**
+ * == BlSubjectIdRunner ==
+ *
+ * @author wada shunsuke
+ * @since  2013/11/22
+ *
+ */
+class BlSubjectIdRunner {
+
+    /**
+     *
+     * @todo
+     * @param args
+     */
+    public static void  main(String[] args) {
+
+    }
+
+}
