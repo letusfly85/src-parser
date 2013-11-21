b91f821f89e707b14900de8288179c1f2faa6294
letusfly85 <letusfly95@gmail.com>
2013-11-22 01:45:53 +0900

    add a test case for BlSubjectIdParser

--- src/test/groovy/com/jellyfish85/srcParser/parser/BlSubjectIdParserTest.groovy
+++ src/test/groovy/com/jellyfish85/srcParser/parser/BlSubjectIdParserTest.groovy
@@ -1,15 +1,10 @@
 package com.jellyfish85.srcParser.parser
 
+import com.jellyfish85.dbaccessor.bean.src.mainte.tool.RsSubjectidBlpathIdxBean
 import com.jellyfish85.svnaccessor.bean.SVNRequestBean
 import org.junit.After
 import org.junit.Before
-import org.w3c.dom.Element
 
-import javax.xml.parsers.DocumentBuilder
-import javax.xml.parsers.DocumentBuilderFactory
-
-import static org.junit.Assert.assertFalse
-import static org.junit.Assert.assertTrue
 import static org.junit.Assert.assertThat
 
 import static org.hamcrest.CoreMatchers.*
@@ -17,45 +12,40 @@ import static org.hamcrest.CoreMatchers.*
 import org.junit.Test
 
 /**
- *
+ * == BlSubjectIdParserTest ==
  *
  * @author wada shunsuke
  * @since  2013/11/22
- * @todo   all
  *
  */
 class BlSubjectIdParserTest {
 
     BlSubjectIdParser parser = new BlSubjectIdParser()
-    DocumentBuilderFactory factory = null
-    DocumentBuilder        db      = null
-
     InputStream inputStream001 = null
-
-    org.w3c.dom.Document   doc001  = null
-    Element                elem001 = null
-
-    org.w3c.dom.Document   doc002  = null
-    Element                elem002 = null
 
 
     @Before
     void setUp() {
         inputStream001 = getClass().getResourceAsStream("/xml/blSubjectIdTest-001.xml")
-        this.doc001       = db.parse(inputStream001)
-        this.elem001      = doc001.getDocumentElement()
+        inputStream001
     }
 
     @After
     void tearDown() {
-        //todo
-
         if (inputStream001 != null) {
             inputStream001.close()
         }
     }
 
+    @Test
     void testParse() {
-        assertTrue(true)
+        SVNRequestBean bean  = new SVNRequestBean()
+
+        ArrayList<RsSubjectidBlpathIdxBean> list = parser.parse(bean, inputStream001)
+
+        RsSubjectidBlpathIdxBean result = list.get(0)
+
+        assertThat(result.subjectGroupIdAttr().value(), is("my.subjectGroupId"))
+        assertThat(result.subjectIdAttr().value(),      is("my.subjectId"))
     }
 }
