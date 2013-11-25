9c695fde9755ff6c75cc92d4ba2133bcea264587
letusfly85 <letusfly95@gmail.com>
2013-11-25 21:30:09 +0900

    add XqlSubjectIdParser test class and its cases

--- src/main/groovy/com/jellyfish85/srcParser/parser/XqlSubjectIdParser.groovy
+++ src/main/groovy/com/jellyfish85/srcParser/parser/XqlSubjectIdParser.groovy
@@ -3,6 +3,11 @@ package com.jellyfish85.srcParser.parser
 import com.jellyfish85.dbaccessor.bean.src.mainte.tool.RsSubjectidXqlpathIdxBean
 import com.jellyfish85.srcParser.utils.ApplicationProperties
 import com.jellyfish85.svnaccessor.bean.SVNRequestBean
+import org.w3c.dom.Element
+import org.xml.sax.SAXParseException
+
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
 
 /**
  * == XqlSubjectIdParser ==
@@ -18,6 +23,8 @@ class XqlSubjectIdParser {
      * == parse ==
      *
      *
+     * @author wada shunsuke
+     * @since  2013/11/25
      * @param app
      * @param list
      * @return
@@ -28,7 +35,76 @@ class XqlSubjectIdParser {
             ApplicationProperties      app,
             ArrayList<SVNRequestBean>  list
     ) throws java.io.IOException, NullPointerException {
+        def workspace = app.workspace()
 
+        def resultSets = new ArrayList<RsSubjectidXqlpathIdxBean>()
+
+        list.each {SVNRequestBean bean ->
+            def file = new File(workspace, bean.path())
+
+            InputStream inputStream = new FileInputStream(file)
+
+            parse(bean, inputStream).each {RsSubjectidXqlpathIdxBean entry -> resultSets.add(entry)}
+
+            inputStream.close()
+        }
     }
 
+    /**
+     * == parse ==
+     *
+     *
+     * @author wada shunsuke
+     * @since  2013/11/25
+     * @param bean
+     * @param inputStream
+     * @return
+     * @throws SAXParseException
+     */
+    public static ArrayList<RsSubjectidXqlpathIdxBean> parse(
+            SVNRequestBean        bean,
+            InputStream           inputStream
+    ) throws SAXParseException {
+
+        def resultSets = new ArrayList<RsSubjectidXqlpathIdxBean>()
+        try {
+            DocumentBuilderFactory factory   = DocumentBuilderFactory.newInstance()
+            DocumentBuilder        db        = factory.newDocumentBuilder()
+            org.w3c.dom.Document   doc       = db.parse(inputStream)
+
+            Element elem                     = doc.getDocumentElement()
+
+            org.w3c.dom.NodeList nodeList = elem.getElementsByTagName("subject")
+
+            if (nodeList.length == 0) {
+                return resultSets
+            }
+
+            for (int i = 0; i < nodeList.length; i++){
+
+                def node = nodeList.item(i)
+                Element entry  = (Element)node
+
+                RsSubjectidXqlpathIdxBean result = new RsSubjectidXqlpathIdxBean()
+                result.pathAttr().setValue(bean.path())
+                result.fileNameAttr().setValue(bean.fileName())
+                result.revisionAttr().setValue(new BigDecimal(bean.revision()))
+                result.subjectIdAttr().setValue(entry.getAttribute("id"))
+
+                result.updateFlgAttr().setValue("0")
+                result.newFlgAttr().setValue("0")
+
+                resultSets.add(result)
+            }
+
+            println("[TARGET]" + bean.path())
+
+        } catch (Exception e) {
+            e.printStackTrace()
+            println("[RUNTIME-ERROR]" + bean.path())
+
+        }
+
+        return resultSets
+    }
 }
--- src/test/groovy/com/jellyfish85/srcParser/parser/XqlSubjectIdParserTest.groovy
+++ src/test/groovy/com/jellyfish85/srcParser/parser/XqlSubjectIdParserTest.groovy
@@ -1,6 +1,6 @@
 package com.jellyfish85.srcParser.parser
 
-import com.jellyfish85.dbaccessor.bean.src.mainte.tool.RsSubjectidBlpathIdxBean
+import com.jellyfish85.dbaccessor.bean.src.mainte.tool.RsSubjectidXqlpathIdxBean
 import com.jellyfish85.svnaccessor.bean.SVNRequestBean
 import org.junit.After
 import org.junit.Before
@@ -12,21 +12,22 @@ import static org.hamcrest.CoreMatchers.*
 import org.junit.Test
 
 /**
- * == BlSubjectIdParserTest ==
+ * == XqlSubjectIdParserTest ==
+ *
  *
  * @author wada shunsuke
- * @since  2013/11/22
+ * @since  2013/11/25
  *
  */
-class BlSubjectIdParserTest {
+class XqlSubjectIdParserTest {
 
-    BlSubjectIdParser parser = new BlSubjectIdParser()
+    XqlSubjectIdParser parser = new XqlSubjectIdParser()
     InputStream inputStream001 = null
-
 
     @Before
     void setUp() {
-        inputStream001 = getClass().getResourceAsStream("/xml/blSubjectIdTest-001.xml")
+        inputStream001 = getClass().getResourceAsStream("/xml/xqlSubjectIdTest-001.xml")
+        inputStream001
     }
 
     @After
@@ -40,11 +41,10 @@ class BlSubjectIdParserTest {
     void testParse() {
         SVNRequestBean bean  = new SVNRequestBean()
 
-        ArrayList<RsSubjectidBlpathIdxBean> list = parser.parse(bean, inputStream001)
+        ArrayList<RsSubjectidXqlpathIdxBean> list = parser.parse(bean, inputStream001)
 
-        RsSubjectidBlpathIdxBean result = list.get(0)
+        RsSubjectidXqlpathIdxBean result = list.get(0)
 
-        assertThat(result.subjectGroupIdAttr().value(), is("my.subjectGroupId"))
         assertThat(result.subjectIdAttr().value(),      is("my.subjectId"))
     }
 }
