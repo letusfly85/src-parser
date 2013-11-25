3f84ee8694efa2ccba42300a8c27a65c25347308
letusfly85 <letusfly95@gmail.com>
2013-11-25 19:06:06 +0900

    add XqlSubjectIdParser

---
+++ src/main/groovy/com/jellyfish85/srcParser/parser/XqlSubjectIdParser.groovy
@@ -0,0 +1,34 @@
+package com.jellyfish85.srcParser.parser
+
+import com.jellyfish85.dbaccessor.bean.src.mainte.tool.RsSubjectidXqlpathIdxBean
+import com.jellyfish85.srcParser.utils.ApplicationProperties
+import com.jellyfish85.svnaccessor.bean.SVNRequestBean
+
+/**
+ * == XqlSubjectIdParser ==
+ *
+ *
+ * @author wada shunsuke
+ * @since  2013/11/25
+ *
+ */
+class XqlSubjectIdParser {
+
+    /**
+     * == parse ==
+     *
+     *
+     * @param app
+     * @param list
+     * @return
+     * @throws IOException
+     * @throws NullPointerException
+     */
+    public static ArrayList<RsSubjectidXqlpathIdxBean> parse(
+            ApplicationProperties      app,
+            ArrayList<SVNRequestBean>  list
+    ) throws java.io.IOException, NullPointerException {
+
+    }
+
+}
