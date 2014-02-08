5da399e45a9a7e4579e285b8dc3bfd646d66835b
letusfly85 <letusfly95@gmail.com>
2014-02-08 22:55:40 +0900

    add stupid.js

--- src/main/groovy/com/jellyfish85/srcParser/runner/SVNDiffViewRunner.groovy
+++ src/main/groovy/com/jellyfish85/srcParser/runner/SVNDiffViewRunner.groovy
@@ -1,6 +1,7 @@
 package com.jellyfish85.srcParser.runner
 
 import com.jellyfish85.srcParser.generator.SVNDiffViewGenerator
+import org.apache.commons.io.FileUtils
 
 class SVNDiffViewRunner {
 
@@ -9,6 +10,11 @@ class SVNDiffViewRunner {
         SVNDiffViewGenerator generator = new SVNDiffViewGenerator()
 
         generator.generate()
+
+        String fileName1 = getClass().getResource("/javascripts/stupidtable.js").getPath()
+        String fileName2 = getClass().getResource("/javascripts/stupidtable.min.js").getPath()
+        FileUtils.copyFile((new File(fileName1)), new File("output", "stupidtable.js"))
+        FileUtils.copyFile((new File(fileName2)), new File("output", "stupidtable.min.js"))
 
     }
 
