93e3757200084dfa1b70d41360dab41204889947
letusfly85 <letusfly95@gmail.com>
2014-02-08 23:00:53 +0900

    add jquery

--- src/main/groovy/com/jellyfish85/srcParser/runner/SVNDiffViewRunner.groovy
+++ src/main/groovy/com/jellyfish85/srcParser/runner/SVNDiffViewRunner.groovy
@@ -13,8 +13,10 @@ class SVNDiffViewRunner {
 
         String fileName1 = getClass().getResource("/javascripts/stupidtable.js").getPath()
         String fileName2 = getClass().getResource("/javascripts/stupidtable.min.js").getPath()
+        String fileName3 = getClass().getResource("/javascripts/jquery-1.8.2.js").getPath()
         FileUtils.copyFile((new File(fileName1)), new File("output", "stupidtable.js"))
         FileUtils.copyFile((new File(fileName2)), new File("output", "stupidtable.min.js"))
+        FileUtils.copyFile((new File(fileName3)), new File("output", "jquery-1.8.2.js"))
 
     }
 
